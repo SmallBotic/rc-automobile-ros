5a53f550874f4a434a6e403c233db36558feb78c
George Ngigi <ngigegeorge023@gmail.com>
2024-07-02 02:41:11 +0300

    ...

--- include/Credentials.example.hpp
+++ include/Credentials.example.hpp
@@ -6,7 +6,9 @@
 #define WIFI_SSID ""
 #define WIFI_PASSWORD ""
 
+// Change this to your ROS2 agent IP
+// (the machine running the ROS2 agent e.g. laptop, raspberry pi, etc.)
 #define ROS_AGENT_IP IPAddress(192, 168, 0, 3)
-#define ROS_AGENT_PORT 8888
+#define ROS_AGENT_PORT 8888 // Default port for ROS2 agent, change if necessary
 
 #endif  // CREDENTIALS_HPP_
